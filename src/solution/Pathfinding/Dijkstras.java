9b4ac59b143c6f832b37ee8fc80c6f4e8fa5654f
txngsten <oliver_wuttke@icloud.com>
2026-06-04 17:28:19 +0930

    Updated Pathfinder.java and added a Dijkstras.java to be a basic implementation of Dijkstras algorithm for pathfinding.

---
+++ src/solution/Pathfinding/Dijkstras.java
@@ -0,0 +1,13 @@
+package solution.Pathfinding;
+
+import solution.Graph;
+import java.util.List;
+import java.util.Optional;
+
+public class Dijkstras implements Pathfinder {
+
+    @Override
+    public Optional<List<String>> getPath(Graph roadMap, String start, String end) {
+        return Optional.empty();
+    }
+}
--- src/solution/Pathfinding/Pathfinder.java
+++ src/solution/Pathfinding/Pathfinder.java
@@ -1,5 +1,9 @@
 package solution.Pathfinding;
 
-public interface Pathfinder {
+import solution.Graph;
+import java.util.List;
+import java.util.Optional;
 
+public interface Pathfinder {
+    Optional<List<String>> getPath(Graph roadMap, String start, String end);
 }
