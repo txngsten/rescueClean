032d3affb68d41428ecaabfc40110d057da857af
txngsten <oliver_wuttke@icloud.com>
2026-06-04 17:15:12 +0930

    Added MyDisasterResponder.java, nothing yet implemented.

---
+++ src/solution/MyDisasterResponder.java
@@ -0,0 +1,20 @@
+package solution;
+
+import sim.Message;
+
+public class MyDisasterResponder extends DisasterResponder {
+
+    MyDisasterResponder() {
+        super();
+    }
+
+    @Override
+    protected void handle(Message s) {
+
+    }
+
+    @Override
+    protected void setup() {
+
+    }
+}
